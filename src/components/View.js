3dd94bcb6facdd32dfb98728f8fcbbed14539d9b
monicasalas86 <monicsalas167@gmail.com>
2021-10-15 18:41:44 -0500

    articles deleting successfully

--- src/components/View.js
+++ src/components/View.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { useHistory } from 'react-router';
 
 import Article from './Article';
 import EditForm from './EditForm';
@@ -11,17 +12,27 @@ const View = (props) => {
     const [articles, setArticles] = useState([]);
     const [editing, setEditing] = useState(false);
     const [editId, setEditId] = useState();
+    const {push} = useHistory();
 
     useEffect(() => {
         articleService()
             .then(res => {
-                console.log(res)
+                // console.log(res)
                 setArticles(res)
             })
             .catch(err=> console.log(err))
     }, [])
 
     const handleDelete = (id) => {
+        axiosWithAuth()
+            .delete(`/articles/${id}`)
+                .then(res => {
+                    
+                    setArticles(articles.filter(article => article.id !== id))
+                })
+                .catch(err => {
+                    console.log(err)
+                })
     }
 
     const handleEdit = (article) => {
